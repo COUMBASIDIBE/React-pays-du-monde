a3b3531aed2d5fab5e7575043971966b35112633
COUMBASIDIBE <coumba.sidibe@hotmail.fr>
2021-12-24 11:02:56 +0100

    commit 1

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => {
+    return(
+
+
+        <div className='logo'>
+            <img src='./img/earth.jpg' alt='logo'/>
+            <h3> LES PAYS DU MONDE </h3>
+        </div>
+    );
+
+};
+
+export default Logo;
---
+++ src/pages/Notfound.js
@@ -0,0 +1,13 @@
+import react from "react";
+const Notfound = ()=> {
+
+    return(
+        <div >
+            <h1> Erreur 404</h1>
+        </div>
+    
+    )
+    
+    }
+    
+    export default Notfound;
